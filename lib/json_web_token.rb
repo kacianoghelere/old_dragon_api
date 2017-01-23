177809a470454d5706dc76f3acef13c6f2b973fc
Kaciano Ghelere <kaciano_ghelere@yahoo.com.br>
2017-01-23 08:58:15 -0200

    Ajustes no JWT

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,10 +1,13 @@
 class JsonWebToken
+
   def self.encode(payload)
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    signature = Rails.application.secrets.token_signature
+    JWT.encode(payload, signature)
   end
 
   def self.decode(token)
-    return HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.secrets.secret_key_base)[0])
+    signature = Rails.application.secrets.token_signature
+    return HashWithIndifferentAccess.new(JWT.decode(token, signature)[0])
   rescue
     nil
   end
