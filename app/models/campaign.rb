c207816bf96ffd97bc73e828c53f66c7ea45ef5d
Kaciano Ghelere <kaciano_ghelere@yahoo.com.br>
2017-08-28 15:34:07 -0300

    Ajuste no model de Campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,6 +2,6 @@ class Campaign < ActiveRecord::Base
   belongs_to :user
   has_many :campaign_journals
   has_many :campaign_members
-  has_many :characters, through: :campaign_members
   has_many :campaign_notes
+  has_many :characters, through: :campaign_members
 end
