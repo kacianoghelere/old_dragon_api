536385b65eba44727fb008b8b7698c5156b1d790
Kaciano Ghelere <themohawkeagle@gmail.com>
2018-02-15 13:13:16 -0200

    Ajuste no serializador de requimentos de classes

--- app/serializers/character_class_requirement_serializer.rb
+++ app/serializers/character_class_requirement_serializer.rb
@@ -1,4 +1,3 @@
 class CharacterClassRequirementSerializer < ActiveModel::Serializer
   attributes :id, :str_mod, :dex_mod, :cons_mod, :int_mod, :wis_mod, :char_mod
-  has_one :character_classes
 end
